c6a7d214f493a8931f9e485a032d0686102f46c7
Gaby Moran <gaby@permanent.company>
2020-02-11 20:00:36 +0700

    Add theme

---
+++ src/theme.tsx
@@ -0,0 +1,27 @@
+import red from '@material-ui/core/colors/red';
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
+
+// A custom theme for this app
+const theme = responsiveFontSizes(
+  createMuiTheme({
+    palette: {
+      primary: {
+        main: '#556cd6',
+      },
+      secondary: {
+        main: '#19857b',
+      },
+      error: {
+        main: red.A400,
+      },
+      background: {
+        default: '#fff',
+      },
+    },
+    typography: {
+      fontWeightRegular: 200,
+    },
+  })
+);
+
+export default theme;
